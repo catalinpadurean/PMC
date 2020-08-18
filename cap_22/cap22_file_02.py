f04093e99ad1b73f2ab27b05929bf0633179db1d
Catalin Padurean <catalinpadurean04@gmail.com>
2020-08-18 11:26:35 +0300

    Add executable file explanation and generated exe file

---
+++ cap_22/cap22_file_02.py
@@ -0,0 +1,7 @@
+"""Create an executable file for this app. The executable file will be ready to use by anybody who has the exe file and not necesary a version of python installed"""
+#install pyinstaller (pip )
+#in the command line just type pyinstaller <file_name>
+#to be practical pass a parameter to the pyinstaller: pyinstaller --onefile <file_name>
+#even more, if you don't want the cmd line to be shown in the background of the GUI, use the --windowed param
+#pyinstaller --onefile --windowed <file_name>
+
