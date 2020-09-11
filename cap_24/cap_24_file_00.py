81a65bb4dd33a16110985f8fecbf081304ce655a
Catalin Padurean <catalinpadurean04@gmail.com>
2020-09-11 20:05:05 +0300

    file 00

---
+++ cap_24/cap_24_file_00.py
@@ -0,0 +1,8 @@
+"""Feel good mobile application"""
+
+#This app will work on Android, iOS, Linux, Windows, macOS
+#The app will have a log in interface or sign up and some buttons to get some quotes from the app
+#We will use kivy module
+# This will also be a fullstack (frontend-backend development)
+
+import kivy
