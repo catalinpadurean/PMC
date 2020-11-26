ef334f635030e31169f86e0f450b0231708d6a57
Catalin Padurean <catalinpadurean04@gmail.com>
2020-11-26 22:04:56 +0200

    Add files for cap 25 APK installer.

---
+++ cap_25/cap_25_file_00.py
@@ -0,0 +1,7 @@
+#Creating an APK for Android. The APK will install the "feel good" app created in cap_24
+#APK is usable only on Android
+#Buildozer is a python library which will be used for creating the APK.
+#Creating an APK file is hard on windows or MacOS and the recommended OS for creating an APK is Linux(Ubuntu)
+#Install Virtual Box to have a virtual machine where we can install Linux
+# www.virtualbox.org to download virtual box installer, after the installation is completed go to ubuntu.com to download an ubuntu version
+#Follow the instructions from the video later. Right now the password isn't working on VM and this chapter doesn't seems so important for me.
